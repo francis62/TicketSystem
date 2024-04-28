5e2e661ba6405ba600e5540cb9ec90e08da9b6db
Tomas Paoletti <81266504+Tomas-Paoletti@users.noreply.github.com>
2024-04-28 19:39:36 -0300

    subiendo routing de paginas tickets,about, agregando componentes about y footer

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -1,24 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms'; // Importa ReactiveFormsModule
+
 import { AppComponent } from './app.component';
-import { TicketComponent } from './componentes/ticket/ticket.component';
-import { ReactiveFormsModule } from '@angular/forms';
 import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { TicketComponent } from './componentes/ticket/ticket.component';
+import { AboutComponent } from './componentes/about/about.component';
+import { routes } from './app.routing.module';
+import { FooterComponent } from './componentes/footer/footer.component'; // Importa FooterComponent
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavbarComponent,
     TicketComponent,
-    NavbarComponent
-  ],
+    AboutComponent,
+    FooterComponent 
+  ], // Remove the FooterComponent from the array of declarations
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpClientModule, 
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
